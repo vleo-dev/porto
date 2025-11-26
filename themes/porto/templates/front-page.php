03e006e7f679a5ff9123ddae7146da179e01284c
fs-leo <lvuylsteker@faire-savoir.com>
2025-11-26 20:41:49 +0100

    Moon effect OK

--- themes/porto/templates/front-page.php
+++ themes/porto/templates/front-page.php
@@ -33,9 +33,6 @@
 		<div class="main-pres__desc">
 			<p><?php echo $main_desc; ?></p>
 
-			<div class="main-pres__desc__scroll">
-				<i class="fas fa-arrow-circle-down"></i>
-			</div>
 		</div>
 		<div class="moon"></div>
 	</section>
@@ -54,7 +51,7 @@
 	<?php if ( !empty($skills) && !is_wp_error($skills) ): ?>
 		<section class="skills" id="skills">
 			<div class="container reveal">
-				<h2><?php echo "Compétences"; ?></h2>
+				<h2 class="dark"><?php echo "Compétences"; ?></h2>
 				<div class="skills-grid">
 					<?php foreach($skills as $skill):
 						$desc = get_field('desc', $skill);
@@ -77,7 +74,7 @@
 
 	<section class="projects">
 		<?php if ( !empty( $webs ) ): ?>
-			<h2><?php echo "Projets"; ?></h2>
+			<h2 class="dark"><?php echo "Projets"; ?></h2>
 			<div class="swiper projectsSwiper">
 				<div class="swiper-wrapper">
 					<?php foreach( $webs as $web ) : ?>
@@ -106,6 +103,7 @@
 
 	<section class="history">
 		<div class="container reveal">
+			<h2 class=""><?php echo "Parcours Pro"; ?></h2>
 			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 		</div>
 	</section>
