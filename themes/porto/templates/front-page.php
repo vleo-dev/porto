f699acdbce4000f7693882091765c9e4de7244e6
vleo-dev <leo.vuylsteker@gmail.com>
2025-11-28 17:28:17 +0100

    Header

--- themes/porto/templates/front-page.php
+++ themes/porto/templates/front-page.php
@@ -103,13 +103,77 @@
 
 	<section class="history">
 		<div class="container reveal">
-			<h2 class=""><?php echo "Parcours Pro"; ?></h2>
-			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+			<h2><?php echo "Parcours Pro"; ?></h2>
+
+			<div class="history__timeline">
+
+			<div class="history__timeline__item">
+				<div class="dot">
+					<div class="dot__image">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/fs-logo.png" alt="Logo" class="round-img">
+					</div>
+				</div>
+				<div class="content">
+					<h3>sept 2017 – En cours</h3>
+					<p>Développeur Full Stack Wordpress / Drupal -- Faire Savoir</p>
+				</div>
+			</div>
+
+			<div class="history__timeline__item">
+				<div class="dot">
+					<div class="dot__image">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/hb-digit.png" alt="Logo" class="round-img">
+					</div>
+				</div>
+				<div class="content">
+					<h3>oct 2016 – août 2017</h3>
+					<p>Développeur Web Drupal -- HB Digit</p>
+				</div>
+			</div>
+
+			<div class="history__timeline__item">
+				<div class="dot">
+					<div class="dot__image">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/auchan.png" alt="Logo" class="round-img">
+					</div>
+				</div>
+				<div class="content">
+					<h3>avr 2016 – aoüt 2018</h3>
+					<p>Stage Développeur WEB -- CGI -- Auchan E - Commerce</p>
+				</div>
+			</div>
+
+			<div class="history__timeline__item">
+				<div class="dot">
+					<div class="dot__image">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/iut.jpg" alt="Logo" class="round-img">
+					</div>
+				</div>
+				<div class="content">
+					<h3>sept 2015 – août 2016</h3>
+					<p>Licence proffessionnelle DAII -- Université de Lille</p>
+				</div>
+			</div>
+
+			<div class="history__timeline__item">
+				<div class="dot">
+					<div class="dot__image">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/bts.png" alt="Logo" class="round-img">
+					</div>
+				</div>
+				<div class="content">
+					<h3>sept 2013 – août 2015</h3>
+					<p>BTS Services Informatiques aux Organisations, Solutions Logicielles et Applications Métiers -- Lycée Saint Rémi ROUBAIX</p>
+				</div>
+			</div>
+
+			</div>
 		</div>
 	</section>
 
 	<section class="contact">
 		<div class="container reveal">
+			<h2 class="dark"><?php echo "Contact"; ?></h2>
 			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 		</div>
 	</section>
--- themes/porto/templates/header.php
+++ themes/porto/templates/header.php
@@ -1,10 +1,21 @@
 <header>
-  <h1>Bienvenue sur mon site web</h1>
-  <nav>
-    <ul>
-      <li><a href="#">Accueil</a></li>
-      <li><a href="#">À propos</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </nav>
+  <div class="header__container">
+    <div class="header__logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/astro-gus.png" alt="logo_cat" title="logo_cat" />
+    </div>
+    <div class="header__nav">
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li>
+            <a href="#">À propos</a>
+            <ul>
+              <li><a href="#">Notre histoire</a></li>
+              <li><a href="#">L'équipe</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+  </div>
 </header>
