649634d647f41ee31cfb18ba8380066a725c81c3
fs-leo <lvuylsteker@faire-savoir.com>
2026-03-23 14:42:33 +0100

    CV 2026

---
+++ themes/porto/src/AppFloatingLines.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="width: 100%; height: 100%; position: relative;">
+    <FloatingLines
+      :line-distance="[8, 6, 4]"
+      :bend-radius="5.0"
+      :bend-strength="-0.5"
+      :interactive="true"
+      :parallax="true"
+      bendStrength="-2"
+    />
+  </div>
+</template>
+
+<script setup>
+import FloatingLines from './FloatingLines.vue'
+</script>
